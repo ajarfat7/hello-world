4a5b4ec44c15b49c00ec581c7f6eed223fe0fa47
ajarfat7 <166813843+ajarfat7@users.noreply.github.com>
2024-04-16 22:38:23 +0500

    Assignment 5

---
+++ ex5.js
@@ -0,0 +1,8 @@
+// Storing a person's name with whitespace characters
+var personName = "\t\n   John Doe   \t\n";
+// Printing the name with whitespace
+console.log("Name with whitespace:");
+console.log(personName);
+// Printing the name after stripping whitespace
+console.log("\nName after stripping whitespace:");
+console.log(personName.trim());
---
+++ ex5.ts
@@ -0,0 +1,10 @@
+
+let personName: string = "\t\n   John Doe   \t\n";
+
+
+console.log("Name with whitespace:");
+console.log(personName);
+
+
+console.log("\nName after stripping whitespace:");
+console.log(personName.trim());
