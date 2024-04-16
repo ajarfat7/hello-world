02208c7687c68cbdc7060fdb9ba38be24413ef5c
ajarfat7 <166813843+ajarfat7@users.noreply.github.com>
2024-04-16 22:34:09 +0500

    Assignment 5

--- ex.js
+++ ex.js
@@ -1,2 +1,8 @@
-var personName = "Eric";
-console.log("Hello ".concat(personName, ", would you like to learn some Python today?"));
+// Storing a person's name with whitespace characters
+var personName = "\t\n   John Doe   \t\n";
+// Printing the name with whitespace
+console.log("Name with whitespace:");
+console.log(personName);
+// Printing the name after stripping whitespace
+console.log("\nName after stripping whitespace:");
+console.log(personName.trim());
