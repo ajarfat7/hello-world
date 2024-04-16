5fb7302c71eb0db1d22af649a2795177e6f803af
ajarfat7 <166813843+ajarfat7@users.noreply.github.com>
2024-04-16 22:40:38 +0500

    Assignment 7

---
+++ ex7.ts
@@ -0,0 +1,11 @@
+// Addition
+console.log("Addition:", 3 + 3);
+
+// Subtraction
+console.log("Subtraction:", 8 - 2);
+
+// Multiplication
+console.log("Multiplication:", 3 * 2);
+
+// Division
+console.log("Division:", 12 / 2);
