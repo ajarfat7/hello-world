c9b8894deb8c819ba98fdfd2a1ee0a92775a48a9
ajarfat7 <166813843+ajarfat7@users.noreply.github.com>
2024-04-16 22:33:38 +0500

    Assignment 4

---
+++ ex4.js
@@ -0,0 +1,3 @@
+var quote = "Be the change that you wish to see in the world.";
+var author = "Mahatma Gandhi";
+console.log("\"".concat(quote, "\" - ").concat(author));
---
+++ ex4.ts
@@ -0,0 +1,4 @@
+let quote: string = "Be the change that you wish to see in the world.";
+let author: string = "Mahatma Gandhi";
+
+console.log(`"${quote}" - ${author}`);
