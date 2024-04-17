f442061027a22a31c7d887b026e0107ef7330fce
ajarfat7 <166813843+ajarfat7@users.noreply.github.com>
2024-04-17 22:36:57 +0500

    Assignment 11

---
+++ ex11.js
@@ -0,0 +1,5 @@
+var FriendNames = ["hassan", "sheeraz", "hamdan"];
+var message = "he is my bestfriend ever";
+console.log(FriendNames[0] + " " + message);
+console.log(FriendNames[1] + " " + message);
+console.log(FriendNames[2] + " " + message);
---
+++ ex11.ts
@@ -0,0 +1,9 @@
+let FriendNames: string [] = [ "hassan","sheeraz" ,"hamdan"]
+let message: string [] = "he is my bestfriend ever"
+  console.log(FriendNames[0] + " " + message )
+  console.log(FriendNames[1] + " " + message )
+  console.log(FriendNames[2] + " " + message )
+
+  
+
+  
