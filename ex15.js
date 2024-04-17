fcb98e6871ea03bcfbb239cad0e50b00cce16d5e
ajarfat7 <166813843+ajarfat7@users.noreply.github.com>
2024-04-17 22:40:07 +0500

    Assignment 15

---
+++ ex15.js
@@ -0,0 +1,10 @@
+var FriendInvite = ["Ali", "Haider", "Aamir", "Bilal"];
+var CanNOTattend = "bilal";
+console.log('"bilal" can’t make the dinner');
+var Newinvited = "Sameer";
+FriendInvite[FriendInvite.indexOf(CanNOTattend)] = Newinvited;
+console.log(FriendInvite);
+FriendInvite.map(function (item) { return console.log("come to my home at night", item); });
+var newpersoninvited = ("Usman");
+FriendInvite[FriendInvite.indexOf(CanNOTattend)] = Newinvited + newpersoninvited;
+console.log("come to my home at night" + " " + (newpersoninvited));
---
+++ ex15.ts
@@ -0,0 +1,10 @@
+let FriendInvite: string []= ["Ali" , "Haider" , "Aamir" , "Bilal"]; 
+let CanNOTattend: string = "bilal";
+console.log('"bilal" can’t make the dinner');
+let Newinvited: string= "Sameer";
+FriendInvite[FriendInvite.indexOf(CanNOTattend)]=Newinvited;
+console.log( FriendInvite);
+FriendInvite.map((item)=> console.log("come to my home at night",item));
+let newpersoninvited: string = ("Usman");
+FriendInvite[FriendInvite.indexOf(CanNOTattend)]=Newinvited + newpersoninvited ;
+console.log(  "come to my home at night" + " " + (newpersoninvited) );
