e98df93bed1bcdca848b5bd6367facd12243d8f7
ajarfat7 <166813843+ajarfat7@users.noreply.github.com>
2024-04-17 22:37:49 +0500

    Assignment 12

---
+++ ex12.js
@@ -0,0 +1,2 @@
+var transportations = ["Honda motorcyles", "Honda city", "Honda civics"];
+transportations.map(function (item) { return console.log("i would like to own a honda motorcycles", item); });
---
+++ ex12.ts
@@ -0,0 +1,2 @@
+let transportations: string []= ["Honda motorcyles" , "Honda city" , "Honda civics"];
+ transportations.map((item)=> console.log("i would like to own a honda motorcycles",item));
