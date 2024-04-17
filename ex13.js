7aeffeef181b05caa15fdfbc3ed0cc9604e19a49
ajarfat7 <166813843+ajarfat7@users.noreply.github.com>
2024-04-17 22:39:32 +0500

    Assignment 13

---
+++ ex13.js
@@ -0,0 +1,2 @@
+var FRIENDInvite = ["Ali", "Haider", "Aamir"];
+FRIENDInvite.map(function (item) { return console.log("come to my home at night", item); });
---
+++ ex13.ts
@@ -0,0 +1,2 @@
+let FRIENDInvite: string []= ["Ali" , "Haider" , "Aamir"]; 
+FRIENDInvite.map((item)=> console.log("come to my home at night",item));
