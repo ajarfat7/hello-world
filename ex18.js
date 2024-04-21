e557badf4d15beda99db13955bc607deb8ddca71
ajarfat7 <166813843+ajarfat7@users.noreply.github.com>
2024-04-21 13:56:54 +0500

    Assignment 18

---
+++ ex18.js
@@ -0,0 +1,3 @@
+var PlacesVisited = ["the Colosseum", "Taj Mahal", "Chichen Itza", "Machu Picchu"];
+console.log(PlacesVisited.sort());
+console.log(PlacesVisited.reverse());
---
+++ ex18.ts
@@ -0,0 +1,10 @@
+// let PlacesVisited: string [] = ["the Colosseum" , "Taj Mahal" , "Chichen Itza", "Machu Picchu" ];
+// console.log(PlacesVisited.sort());
+// console.log(PlacesVisited.reverse());
+
+
+
+ let shortAerry: ["the Colosseum" , "Taj Mahal" , "Chichen Itza", "Machu Picchu" ];
+ shortAerry.sort();
+ console.log(shortAerry);
+ 
