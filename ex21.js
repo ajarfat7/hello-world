7f70a367e6fcbaa5b77f82f4bfaf7f576273689c
ajarfat7 <166813843+ajarfat7@users.noreply.github.com>
2024-04-21 15:47:40 +0500

    Assignment 21

---
+++ ex21.js
@@ -0,0 +1,14 @@
+var countriesAndCapitals = {
+    "United States": "Washington, D.C.",
+    "United Kingdom": "London",
+    "France": "Paris",
+    "Germany": "Berlin",
+    "Japan": "Tokyo",
+    "India": "New Delhi",
+    "Brazil": "Brasília",
+    "Australia": "Canberra",
+    "Italy": "Rome",
+    "China": "Beijing"
+};
+console.log("Countries and Their Capitals:");
+console.log(countriesAndCapitals);
---
+++ ex21.ts
@@ -0,0 +1,18 @@
+
+let countriesAndCapitals: { [country: string]: string } = {
+    "United States": "Washington, D.C.",
+    "United Kingdom": "London",
+    "France": "Paris",
+    "Germany": "Berlin",
+    "Japan": "Tokyo",
+    "India": "New Delhi",
+    "Brazil": "Brasília",
+    "Australia": "Canberra",
+    "Italy": "Rome",
+    "China": "Beijing"
+  };
+  
+
+  console.log("Countries and Their Capitals:");
+  console.log(countriesAndCapitals);
+  
