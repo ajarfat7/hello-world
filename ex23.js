803fd985a1d79a2c683ff683d9c4388ee8844ff1
ajarfat7 <166813843+ajarfat7@users.noreply.github.com>
2024-04-21 15:57:32 +0500

    Assignment 23

---
+++ ex23.js
@@ -0,0 +1,32 @@
+var number = 10;
+// Test 1: Is number equal to 10?
+console.log("Is number == 10? I predict True.");
+console.log(number == 10); // True
+// Test 2: Is number not equal to 5?
+console.log("Is number != 5? I predict True.");
+console.log(number != 5); // True
+// Test 3: Is number less than or equal to 20?
+console.log("Is number <= 20? I predict True.");
+console.log(number <= 20); // True
+// Test 4: Is number greater than 5?
+console.log("Is number > 5? I predict True.");
+console.log(number < 5); // false
+// Test 5: Is number divisible by 2?
+console.log("Is number % 2 == 0? I predict True.");
+console.log(number % 2 == 0); // True
+var fruit = 'apple';
+// Test 6: Is fruit equal to 'apple'?
+console.log("Is fruit == 'apple'? I predict True.");
+console.log(fruit == 'apple'); // True
+// Test 7: Is fruit not equal to 'banana'?
+console.log("Is fruit != 'banana'? I predict True.");
+console.log(fruit != 'banana'); // True
+// Test 8: Is fruit length greater than 5?
+console.log("Is fruit.length > 5? I predict False.");
+console.log(fruit.length > 5); // False
+// Test 9: Is fruit starts with 'a'?
+console.log("Does fruit start with 'a'? I predict True.");
+console.log(fruit.startsWith('a')); // True
+// Test 10: Is fruit uppercase?
+console.log("Is fruit uppercase? I predict False.");
+console.log(fruit.toUpperCase() === fruit); // False
