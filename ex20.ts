b003f90fb44d554548ecd6843d25e105b949bc29
ajarfat7 <166813843+ajarfat7@users.noreply.github.com>
2024-04-21 13:59:21 +0500

    Assignment 20

---
+++ ex20.ts
@@ -0,0 +1,16 @@
+// Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+let landmarks: string[] = [
+    "Eiffel Tower",
+    "Great Wall of China",
+    "Taj Mahal",
+    "Statue of Liberty",
+    "Machu Picchu",
+    "Pyramids of Giza",
+    "Sydney Opera House",
+    "Colosseum",
+    "Stonehenge",
+    "Mount Everest"
+];
+
+console.log("List of Famous Landmarks:");
+console.log(landmarks);
