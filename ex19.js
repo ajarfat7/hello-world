b5f76a7cba8572e20024f8031c8e33b5b16741b3
ajarfat7 <166813843+ajarfat7@users.noreply.github.com>
2024-04-21 13:58:46 +0500

    Assignment 19

---
+++ ex19.js
@@ -0,0 +1,2 @@
+var guests = ["Ali", "Haider", "Aamir", "Bilal"];
+console.log("You are inviting ".concat(guests.length, " people to dinner."));
---
+++ ex19.ts
@@ -0,0 +1,2 @@
+let guests: string []= ["Ali" , "Haider" , "Aamir" , "Bilal"]; 
+console.log(`You are inviting ${guests.length} people to dinner.`);
