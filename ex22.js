fc71582a3d5be1a9a99545ff5c6286c6a8e1fcf3
ajarfat7 <166813843+ajarfat7@users.noreply.github.com>
2024-04-21 15:57:03 +0500

    Assignment 22

---
+++ ex22.js
@@ -0,0 +1,6 @@
+var number1 = [1, 2, 3, 4, 5];
+var index1 = 2;
+console.log(number1[index1]);
+var numbers2 = [1, 2, 3, 4, 5];
+var index2 = 10;
+console.log(numbers2[index2]);
---
+++ ex22.ts
@@ -0,0 +1,7 @@
+let number: number [] = [1 , 2 , 3 , 4 , 5];
+
+let index = 2;
+
+console.log(number[index]);
+
+
