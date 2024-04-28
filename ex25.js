2983498bd44a82b48a677d6fd4632f4cbe1da59c
ajarfat7 <166813843+ajarfat7@users.noreply.github.com>
2024-04-28 14:02:37 +0500

    Assignment 25

---
+++ ex25.js
@@ -0,0 +1,8 @@
+var alien_color = 'green';
+if (alien_color === 'green') {
+    console.log("The player just earned 5 points.");
+}
+var alien_color2 = 'red';
+if (alien_color2 === 'green') {
+    console.log("The player no earned  points.");
+}
---
+++ ex25.ts
@@ -0,0 +1,13 @@
+let alien_color = 'green';
+
+if (alien_color === 'green') {
+    console.log("The player just earned 5 points.");
+}
+
+
+
+let alien_color2 = 'red';
+
+if (alien_color2 === 'green') {
+    console.log("The player just earned 10 points.");
+}
