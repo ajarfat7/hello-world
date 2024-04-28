20e1c63ec928c6f48bd5ed889b896c91a35db625
ajarfat7 <166813843+ajarfat7@users.noreply.github.com>
2024-04-28 14:14:20 +0500

    Assignment  36

---
+++ ex36.ts
@@ -0,0 +1,5 @@
+function make_shirt(size: string, message: string): void {
+    console.log(`The shirt size is ${size.toUpperCase()} and the message printed on it is: "${message}".`);
+}
+
+make_shirt("large", "Hello, World!");
