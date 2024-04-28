cbb093849492acf9c8be8639a25a069b3bf16443
ajarfat7 <166813843+ajarfat7@users.noreply.github.com>
2024-04-28 14:03:27 +0500

    Assignment 27

---
+++ ex27.ts
@@ -0,0 +1,13 @@
+let aliencolour = "yellow"
+
+if (aliencolour === "green"){
+    console.log("that the player earned 5 points");
+}
+
+if (aliencolour === "yellow"){
+    console.log("that the player earned 10 points");
+}
+
+if (aliencolour === "red"){
+    console.log("that the player earned 15 points");
+}
