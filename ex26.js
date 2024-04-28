f80918166ce22c72d2834325ee21fa93c98c3949
ajarfat7 <166813843+ajarfat7@users.noreply.github.com>
2024-04-28 14:03:00 +0500

    Assignment 26

---
+++ ex26.js
@@ -0,0 +1,4 @@
+var aliencolor = "green";
+if (aliencolor !== "green") {
+    console.log("You lose 5 points shooting the green alien");
+}
---
+++ ex26.ts
@@ -0,0 +1,10 @@
+let aliencolor = "green"  
+
+if (aliencolor == "green"){
+    console.log("Congratulations! You just earned 5 points for shooting the green alien.")
+} 
+
+if (aliencolor !== "green"){
+    console.log("You lose 5 points for shooting the green alien")
+
+}
