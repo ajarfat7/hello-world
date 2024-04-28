2012f6a302067453b96dd0442c37b2f65c658b50
ajarfat7 <166813843+ajarfat7@users.noreply.github.com>
2024-04-28 14:04:24 +0500

    Assignment 28

---
+++ ex28.js
@@ -0,0 +1,21 @@
+var age = 70;
+if (age < 5) {
+    console.log("that the person is a baby.");
+}
+else if (age < 10) {
+    console.log("that the person is a toddler.");
+}
+else if (age < 15) {
+    console.log("that the person is a kid.");
+}
+else if (age < 20) {
+    console.log("that the person is a teenager.");
+}
+else if (age < 65) {
+    console.log("that the person is a adult.");
+}
+else
+    (age < 100);
+{
+    console.log("that the person is a elder.");
+}
